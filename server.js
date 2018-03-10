c876497e25728cc28e6eb21462f84ae819973000
ixnp <enprocopios@gmail.com>
2018-03-10 14:04:09 -0800

    Chat Server up

--- server.js
+++ server.js
@@ -10,24 +10,32 @@ const server = net.createServer();
 
 const pool = [];
 
+ee.on('@nickname', (client, string)=>{
+    let nickname = string.split(' ').shift().trim();
+    client.nickname = nickname;
+    client.socket.write(`nickname updated to ${nickname}`);
+});
+
 server.on('connection', function(socket){
     var client = new Client(socket);
     pool.push(client);
     console.log(client.nickname);
-
     socket.on('data', function(data){
-        const command = data.toString().split('').shift().trim();
+        const command = data.toString().split(' ').shift().trim();
+        const string = data.toString().split(' ').splice(1).join(' ');
         if(command.startsWith('@')){
-            ee.emit(command, client, data.toString().split(' ').splice(1).join(' '));
-            console.log('yaaaaa')
+            ee.emit(command, client, string);
+           console.log('event data', client.nickname);
             return;
         }
-
-        ee.emit('default');
-        console.log('command:', command);
+        ee.emit('defualt');
+        console.log(client.nickname + ':' + command);
     });
 });
+
 
 server.listen(PORT,()=>{
     console.log(`listening on ${PORT}`);
 });
+
+
